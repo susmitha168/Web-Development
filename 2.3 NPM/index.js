0935e28b9cdeabd3609410fe6eb28b49400c6716
susmitha168 <sushmitha.vajja@yahoo.com>
2023-12-20 19:17:40 +0530

    NPM

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,10 @@
+//var generateName = require("sillyname");
+// import generateName from "sillyname";
+//var sillyName = generateName();
+
+//console.log(`My name is ${sillyName}.`);
+
+import superheros from "superheros";
+
+const name = superheros.random();
+console.log(`I am ${name}!`);
